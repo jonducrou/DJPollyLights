af2b2e6ce93d1a86eb234e0be706f92a2325b5ea
Jon Ducrou <jonducrou@gmail.com>
2000-01-01 01:53:30 +0000

    Add smooth transitions.

--- core.js
+++ core.js
@@ -4,7 +4,7 @@ var r = require('./vizs/rainbow.js');
 var t = require('./vizs/text.js');
 var im = require('./vizs/image.js');
 var skulls = require('./vizs/skulls.js');
-var plasma = require('./vizs/plasma.js');
+var plasma = require('./vizs/plasma2.js');
 var fire = require('./vizs/fire.js');
 var equalizer = require('./vizs/equalizer.js');
 var glasses = require('./vizs/glasses.js');
@@ -12,9 +12,11 @@ var hearts = require('./vizs/hearts.js');
 var clock = require('./vizs/clock.js');
 var dj_polly = require('./vizs/dj-polly.js');
 var surf = require('./vizs/surf.js');
+var mo = require('./vizs/mo.js');
+var mario = require('./vizs/mario.js');
 var FRAME_RATE = 50;
 var c = 0;
-var now_playing = ['dj_polly'];
+var now_playing = [['dj_polly',255]];
 
 var pixels = new Array();
 for (var x = 0; x < 24;++x){
@@ -40,7 +42,9 @@ var providers = {
   'hearts': new hearts(),
   'clock': new clock(),
   'dj_polly': new dj_polly(),
-  'surf': new surf()
+  'surf': new surf(),
+  'mo': new mo(),
+  'mario': new mario()
 };
 
 var next_frame = (new Date).getTime();
@@ -52,18 +56,30 @@ function go() {
   next_frame = d+1000/FRAME_RATE;
   
   c++;
-  if (c > 250) {
-    console.log("avg " + miss_c/c + "ms late " + now_playing);
-    if (now_playing.length > 2 || (now_playing.length == 2 && Math.random() > .5)) {
-      now_playing.splice(Math.floor(Math.random() * now_playing.length), 1);
-    } else {
+  if (now_playing[0][1] <= 0) {
+    now_playing.splice(0,1);
+  }
+  if (c > 500) {
+    console.log("avg " + miss_c/c + "ms per frame (" + now_playing + ")");
+    if (now_playing.length == 1) {
       var keys = Object.keys(providers);
-      now_playing.push(keys[ keys.length * Math.random() << 0]);
+      for (var p=0;p<now_playing.length;++p){
+        for (var k=0;k<keys.length;++k){
+          if (p[0] == keys[k]) {
+            keys.splice(k,1);
+          }
+        }
+      }
+      now_playing.push([keys[ keys.length * Math.random() << 0], 0]);
     }
     c=0;
     miss_c=0;
   }
-  
+  if (now_playing.length > 1) {
+    now_playing[0][1]-=now_playing[0][1]<=0?0:1;
+    now_playing[1][1]+=now_playing[1][1]>=255?0:1;
+  }
+
   var s;
   try {
     s = spectrum.getSpectrum();
@@ -90,16 +106,15 @@ function go() {
       }
     }
   }
-//  var things = ['skulls','plasma','text','rainbow'];
-  var things = ['surf'];//'dj_polly','fire'];//'equalizer','glasses'];//'fire','text','rainbow','plasma'];
+  var things = [['mario',255]];
   things = now_playing;
   
   for (var i = 0; i < things.length; ++i){
-    var v = providers[things[i]].getFrame(s,volume,s[8] > 0);
+    var v = providers[things[i][0]].getFrame(s,volume,s[7],s[8] > 0);
     for (var x = 0; x < 24;++x){
       for (var y = 0; y < 6;++y){
         for (var b = 0; b < 3; ++b){
-          pixels[x][y][b] += v[x][y][b]/things.length;
+          pixels[x][y][b] += v[x][y][b]*things[i][1]/255;
         }  
       }
     }
