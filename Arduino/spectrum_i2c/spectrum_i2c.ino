24d24055fd727c22b559e8b7b9548cc0d9c7daba
Jon Ducrou <jonducrou@gmail.com>
2013-10-08 14:16:41 -0700

    Add Arduino spectum code

---
+++ Arduino/spectrum_i2c/spectrum_i2c.ino
@@ -0,0 +1,60 @@
+#include "Wire.h"
+
+int spectrumReset=5;
+int spectrumStrobe=4;
+int spectrumAnalog=0;  
+
+byte spectrum[7];
+
+void setup() { 
+  Wire.begin(0x29);              
+  Wire.onRequest(sendSpectrum); 
+  
+  pinMode(spectrumReset, OUTPUT);
+  pinMode(spectrumStrobe, OUTPUT);
+
+  //Init spectrum analyzer
+  digitalWrite(spectrumStrobe,LOW);
+  delay(1);
+  digitalWrite(spectrumReset,HIGH);
+  delay(1);
+  digitalWrite(spectrumStrobe,HIGH);
+  delay(1);
+  digitalWrite(spectrumStrobe,LOW);
+  delay(1);
+  digitalWrite(spectrumReset,LOW);
+  delay(5);
+  // Reading the analyzer now will read the lowest frequency.
+}
+
+//just loop and read - need to add normalization
+void loop() {
+  readSpectrum();
+  delay(10);
+}
+
+// Read 7 band equalizer.
+void readSpectrum()
+{
+  // Band 0 = Lowest Frequencies.
+  byte i;
+  for(i=0;i <7; i++)
+  {
+    //Read twice and take the average by dividing by 2
+    int value = (analogRead(spectrumAnalog) + analogRead(spectrumAnalog) ) >>1; 
+    //there is noise on the line - this just smooths the noise by flooring low signal strength to zero
+    value -= 50;
+    if (value < 0)
+      value=0;
+    //value is 10 bit, we need 8 bit, so divide by 4
+    value >>= 2;
+    spectrum[i] = value;
+    //request the next value
+    digitalWrite(spectrumStrobe,HIGH);
+    digitalWrite(spectrumStrobe,LOW);     
+  }
+}
+
+void sendSpectrum() {
+  Wire.write(spectrum, 7);
+}
