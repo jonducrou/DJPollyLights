230726aaf761c3604fe6c5182cf4542fbf5757f5
Amanda Ducrou <amanda@calculon.(none)>
2013-10-12 20:19:44 -0700

    Proper fix for upside-down equalizer, take 2

--- vizs/equalizer.js
+++ vizs/equalizer.js
@@ -27,16 +27,16 @@ equalizer.prototype.getFrame = function(spectrum,volume){
   for (var x = 0; x < 24; x += 4) {
     var i = x / 4;
     var v = Math.floor(spectrum[i] * ( 6 / 256 ));
-    for (var y = 5; y >= 0; y--) {
+    for (var y = 0; y < 6; y++) {
       if (v > y) {
 	// Each bar is 4 pixels wide
 	for (var p = 0; p < 4; p++) {
-	  this.pixels[x+p][y] = eqmap[y];
+	  this.pixels[x+p][5 - y] = eqmap[5 - y];
 	}
       } else {
 	// Each bar is 4 pixels wide
 	for (var p = 0; p < 4; p++) {
-	  this.pixels[x+p][y] = [0, 0, 0];
+	  this.pixels[x+p][5 - y] = [0, 0, 0];
 	}
       }
     }
