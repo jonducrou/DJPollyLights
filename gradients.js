93f1dfa8a7425a79e1b3a3fc31072262eb78adc5
Jon Ducrou <jonducrou@gmail.com>
2013-10-13 05:31:12 +0000

    Add gradient test

---
+++ gradients.js
@@ -0,0 +1,19 @@
+var leds = require('./24x6');
+var c = 0;
+for (var x = 0; x < 24;++x){
+  var input = 255*x/23;
+  console.log(input, " - ", Math.pow(input,1.5)/24); 
+  leds.setLed(x,0,Math.pow(input,1.5)/24,0,0);
+ 
+  leds.setLed(x,1,255,0,0); 
+  leds.setLed(x,2,0,128*x/24,0,0); 
+  leds.setLed(x,3,0,255,0,0); 
+  leds.setLed(x,4,0,0,128*x/24,0,0); 
+  leds.setLed(x,5,0,0,255,0,0); 
+
+  for (var y = 0; y < 3;++y){ 
+    c++;
+//    leds.setLed(x,y,c,c,c); 
+  }
+}
+leds.draw();
